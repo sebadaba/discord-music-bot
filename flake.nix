627d1443911999b02c1fa45bd1b84e814b7bed94
Sebastian Torres Valenta <s.torresval+github@protonmail.com>
2025-02-18 15:54:21 -0300

    nix shell with flakes and direnv

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        gcc
+        cmake
+        dpp
+        openssl
+      ];
+
+      shellHook = ''
+      '';
+    };
+  };
+}
